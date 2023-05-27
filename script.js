95fdd943a5c7dbab06823282729a564d20744eb4
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-05-27 18:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+var team = players;
+var team1 = players.slice(); // Creating a shallow copy of players
+var cap1 = Object.assign({}, person); // Creating a shallow copy of person
+
+console.log(team);   // Output: ['John', 'Mike', 'Sarah']
+console.log(team1);  // Output: ['John', 'Mike', 'Sarah']
+console.log(cap1);   // Output: { name: 'Alice', age: 25, city: 'New York' }
+
 window.players = players;
 window.person = person;
 window.team = team;
